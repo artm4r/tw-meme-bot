95bfec394e611ef850078ba4232b5492e7446fc6
Artem Marakutsa <artmar14@gmail.com>
2021-07-07 21:02:40 +0300

    fix #1

--- index.js
+++ index.js
@@ -4,59 +4,59 @@ const axios = require('axios');
 
 let currentToken = '';
 
-async function getAccessToken() {
-   let result = await axios({
-        method: 'POST',
-        url: 'https://id.twitch.tv/oauth2/token',
-        params: {
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET,
-            grant_type: 'refresh_token',
-            refresh_token: process.env.CLIENT_REFRESH_TOKEN,
-        }
-    });
-    return await result.data.access_token;
-}
-
-async function getChannelId(channelName) {
-    let result = await axios({
-         method: 'GET',
-         url: 'https://api.twitch.tv/kraken/users/',
-         params: {
-             api_version: '5',
-             client_id: process.env.CLIENT_ID,
-             login: channelName,
-         }
-     });
-
-    return await result.data.users[0]['_id'];
-}
-
-
-async function makeClip(channel) {
-    let token, channelId;
-    await getAccessToken().then((res)=>{token = res});
-    await getChannelId(channel).then((res)=>{channelId = res});
-
-    let result = await axios({
-        method: 'POST',
-        url: 'https://api.twitch.tv/helix/clips',
-        params: {
-            broadcaster_id: channelId,
-        },
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Client-Id' : `${process.env.CLIENT_ID}`
-        }
-    });
-
-    return await result.data.data[0].edit_url;
-}
+// async function getAccessToken() {
+//    let result = await axios({
+//         method: 'POST',
+//         url: 'https://id.twitch.tv/oauth2/token',
+//         params: {
+//             client_id: process.env.CLIENT_ID,
+//             client_secret: process.env.CLIENT_SECRET,
+//             grant_type: 'refresh_token',
+//             refresh_token: process.env.CLIENT_REFRESH_TOKEN,
+//         }
+//     });
+//     return await result.data.access_token;
+// }
+//
+// async function getChannelId(channelName) {
+//     let result = await axios({
+//          method: 'GET',
+//          url: 'https://api.twitch.tv/kraken/users/',
+//          params: {
+//              api_version: '5',
+//              client_id: process.env.CLIENT_ID,
+//              login: channelName,
+//          }
+//      });
+//
+//     return await result.data.users[0]['_id'];
+// }
+//
+//
+// async function makeClip(channel) {
+//     let token, channelId;
+//     await getAccessToken().then((res)=>{token = res});
+//     await getChannelId(channel).then((res)=>{channelId = res});
+//
+//     let result = await axios({
+//         method: 'POST',
+//         url: 'https://api.twitch.tv/helix/clips',
+//         params: {
+//             broadcaster_id: channelId,
+//         },
+//         headers: {
+//             'Authorization': `Bearer ${token}`,
+//             'Client-Id' : `${process.env.CLIENT_ID}`
+//         }
+//     });
+//
+//     return await result.data.data[0].edit_url;
+// }
 const client = new tmi.Client({
     options: { debug: false },
     identity: {
-        username: process.env.TWITCH_NICK,
-        password: process.env.TWITCH_PASS,
+        username: 'artmar_bot',
+        password: 'oauth:e8b6ww8c0n9qcmdp7v8ezyihwhixx7',
     },
     channels: ['djedybream']
 });
